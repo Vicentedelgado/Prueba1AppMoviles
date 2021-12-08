3f56c4261da1efce9d6d2abc44c00e5feac92a50
Vicentedelgado <vicente.delgado@epn.edu.ec>
2021-12-08 14:04:32 -0500

    primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Prueba1-CHAT',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
